30616167e05658d5d5961ba07eb277cb2536b54c
dativardzma <dati.svanidze123@gmail.com>
2025-04-05 22:14:19 +0400

    changed product serializers

--- products/serializers.py
+++ products/serializers.py
@@ -6,23 +6,35 @@ class CharacteristicSerializer(serializers.ModelSerializer):
         model = Characteristic
         fields = '__all__'
 
-class ProductSerializer(serializers.ModelSerializer):
-    characteristic = CharacteristicSerializer(many=True)
-    images = serializers.ListField(child=serializers.ImageField(), write_only=True)
-
-    class Meta:
-        model = Product
-        fields = ['name', 'price', 'percent', 'characteristic', 'images']
-
-    def create(self, validated_data):
-        images = validated_data.pop("images")
-        product = Product.objects.create(name=validated_data["name"])
-        for image in images:
-            ProductImage.objects.create(product=product, image=image)
-        return product
-
 class ProductImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductImage
         fields = ["image"]
 
+class ProductSerializer(serializers.ModelSerializer):
+    characteristic = CharacteristicSerializer(many=True)
+    images = ProductImageSerializer(many=True, read_only=True)
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(), write_only=True, required=False
+    )
+
+    class Meta:
+        model = Product
+        fields = ['name', 'price', 'percent', 'characteristic', 'images', 'uploaded_images']
+
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images", [])
+        characteristics_data = validated_data.pop("characteristic", [])
+        
+        product = Product.objects.create(**validated_data)
+
+        # Handle ManyToMany Characteristic
+        for char_data in characteristics_data:
+            char_instance, created = Characteristic.objects.get_or_create(**char_data)
+            product.characteristic.add(char_instance)
+
+        # Handle Image Uploads
+        for image in uploaded_images:
+            ProductImage.objects.create(product=product, image=image)
+
+        return product
