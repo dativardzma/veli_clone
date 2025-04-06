a814449b2bc18f8f99ee3a3de70a082ee6634f29
dativardzma <dati.svanidze123@gmail.com>
2025-04-06 21:06:25 +0400

    added filtering

--- products/models.py
+++ products/models.py
@@ -20,4 +20,4 @@ class Product(models.Model):
     characteristic = models.ManyToManyField(Characteristic)
     image = models.ImageField(upload_to='product_images/')
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    date_added = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(auto_now_add=True) 
--- products/views.py
+++ products/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import Product 
+from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.generics import ListCreateAPIView
 
-# Create your views here.
+
 class ProductView(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        filt = self.request.query_params.get('filter', None)
+
+        if filt == "date":
+            return Product.objects.all().order_by('-date_added')
+        if filt == "discount":
+            return Product.objects.all().order_by('-percent')
+        if filt == "Price_Descending":
+            return Product.objects.all().order_by('-price')
+        if filt == "Price_Ascending":
+            return Product.objects.all().order_by('price')
+        
+        return Product.objects.all()
