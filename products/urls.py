8f6a1bc0a697f9d73a483b1058b764553091738d
dativardzma <dati.svanidze123@gmail.com>
2025-04-08 11:40:02 +0400

    made filtering and filter swagger

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductView
+from .views import ProductView, ProductFilter
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
@@ -8,7 +8,8 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 router = DefaultRouter()
-router.register(r"", ProductView, basename='products')
+router.register(r"products", ProductView, basename='products')
+router.register(r"category", ProductFilter, basename='products_filter')
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -24,7 +25,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path("products/", include(router.urls)),
+    path("", include(router.urls)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
--- products/views.py
+++ products/views.py
@@ -3,22 +3,49 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.generics import ListCreateAPIView
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from rest_framework.response import Response
 
 
 class ProductView(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+class ProductFilter(ModelViewSet):
+    serializer_class = ProductSerializer
+
+    @swagger_auto_schema(
+        operation_description="Retrieve a list of products",
+        manual_parameters=[
+            openapi.Parameter('category', openapi.IN_QUERY, description="Filter by category", type=openapi.TYPE_STRING),
+            openapi.Parameter('filter', openapi.IN_QUERY, description="Sort by date, price, etc.", type=openapi.TYPE_STRING)
+        ],
+        responses={200: ProductSerializer(many=True)}
+    )
+
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     def get_queryset(self):
+        category = self.request.query_params.get('category', None)
         filt = self.request.query_params.get('filter', None)
 
+        queryset = Product.objects.all() 
+
+        if category:
+            if category.isdigit():
+                queryset = queryset.filter(category__id=int(category))
+            else:
+                queryset = queryset.filter(category__name__iexact=category) 
+
         if filt == "date":
-            return Product.objects.all().order_by('-date_added')
-        if filt == "discount":
-            return Product.objects.all().order_by('-percent')
-        if filt == "Price_Descending":
-            return Product.objects.all().order_by('-price')
-        if filt == "Price_Ascending":
-            return Product.objects.all().order_by('price')
-        
-        return Product.objects.all()
+            queryset = queryset.order_by('-date_added')
+        elif filt == "discount":
+            queryset = queryset.order_by('-percent')
+        elif filt == "Price_Descending":
+            queryset = queryset.order_by('-price')
+        elif filt == "Price_Ascending":
+            queryset = queryset.order_by('price')
+
+        return queryset
