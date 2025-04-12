41c3066374b436f04b60d0faba1c47feed3a6997
dativardzma <dati.svanidze123@gmail.com>
2025-04-12 15:02:46 +0400

    edited views.py

--- products/views.py
+++ products/views.py
@@ -18,6 +18,7 @@ class ProductView(ModelViewSet):
     lookup_field = 'id'
 
 class ProductFilter(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = CategorySerializer
 
     @swagger_auto_schema(
